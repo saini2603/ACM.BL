36373e92bb4f644a35b7a05b623644d9635f4b3b
user <user@DESKTOP-7L1C8IJ>
2022-01-21 19:09:12 +0530

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+     
+    public class Customer
+    {
+        public string CustomerID { get; private set; }
+
+       
+        public string emailAddress { get; private set; }
+        private string _lastname;
+        private string _firstName;
+
+        public string Fullname {
+            get
+            {
+                return lastname + "," + firstname;
+            }
+                 }
+
+        public string firstname
+        {
+            get { return _firstName; }
+
+
+            set { _lastname = value; }
+
+        }
+        public string lastname
+        {
+            get { return _lastname; }
+
+
+            set { _lastname = value; }
+
+        }
+    }
+}
